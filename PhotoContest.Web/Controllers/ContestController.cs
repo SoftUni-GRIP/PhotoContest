29bc958575961be911fbff82446fd13d8fe7d8a4
benadaephondelat <georgi_iliev@yahoo.com>
2015-10-18 17:17:10 -0700

    Contest Controller(forgot to add it the first time)

---
+++ PhotoContest.Web/Controllers/ContestController.cs
@@ -0,0 +1,43 @@
+﻿namespace PhotoContest.Web.Controllers
+{
+    using System.Web.Mvc;
+    using AutoMapper;
+    using Data.Contracts;
+    using PhotoContest.Models;
+    using Models.ContestModels.InputModels;
+
+    [Authorize]
+    public class ContestController : BaseController
+    {
+        public ContestController(IPhotoContestData data)
+            : base(data)
+        {
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(CreateContestInputModel model)
+        {
+            //TODO: Extract check for model state in a Filter above thee
+
+            if (this.ModelState.IsValid && model != null)
+            {
+                var contest = Mapper.Map<CreateContestInputModel, Contest>(model);
+                contest.OwnerId = this.CurrentUser.Id;
+
+                this.Data.Contests.Add(contest);
+                this.Data.SaveChanges();
+
+                //TODO: Remove magic strings
+                return this.RedirectToAction("Index", "Home");
+            }
+
+            return View(model);
+        }
+    }
+}
