a1b2d2537e612f0b45c8b5a42662e4bc13e10a5f
ibakyrdjiev <ibakyrdjiev@gmail.com>
2015-10-21 07:37:03 -0800

    missing function for modals - need to be refactored for reusing

---
+++ PhotoContest.Web/Scripts/votemodal.js
@@ -0,0 +1,12 @@
+﻿function modal() {
+    $("a[data-modal]").on("click", function () {
+        $("#vote-modal-content").load(this.href, function () {
+            $("#vote-modal").modal({ keyboard: true }, "show");
+        });
+        return false;
+    });
+}
+
+modal();
+
+
