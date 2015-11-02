5a638848afbd0c946d18853c72f89e03f94dd9fe
benadaephondelat <georgi_iliev@yahoo.com>
2015-11-02 14:36:25 +0200

    My Contests Improvements
    Added My Participations page
    Added My Votes page

--- PhotoContest.Web/Controllers/MyContestController.cs
+++ PhotoContest.Web/Controllers/MyContestController.cs
@@ -8,6 +8,7 @@
     using Models.ContestModels.ViewModels;
     using Models.HomeControllerModels;
 
+    [Authorize]
     public class MyContestController : BaseController
     {
         private ICacheService cache;
@@ -20,6 +21,40 @@
         public ActionResult Index()
         {
             var contests = this.cache.Contests.Where(c => c.OwnerId == this.CurrentUser.Id)
+                                              .AsQueryable()
+                                              .Project()
+                                              .To<ContestBasicDetails>()
+                                              .ToList();
+
+            var model = new HomePageViewModel()
+            {
+                ContestBasicDetails = contests,
+                CurrentUserId = this.CurrentUser == null ? null : this.CurrentUser.Id,
+            };
+
+            return View(model);
+        }
+
+        public ActionResult Participated()
+        {
+            var contests = this.cache.Contests.Where(c => c.Participants.Any(p => p.Id == this.CurrentUser.Id))
+                                              .AsQueryable()
+                                              .Project()
+                                              .To<ContestBasicDetails>()
+                                              .ToList();
+
+            var model = new HomePageViewModel()
+            {
+                ContestBasicDetails = contests,
+                CurrentUserId = this.CurrentUser == null ? null : this.CurrentUser.Id,
+            };
+
+            return View(model);
+        }
+
+        public ActionResult Voted()
+        {
+            var contests = this.cache.Contests.Where(c => c.Voters.Any(v => v.Id == this.CurrentUser.Id))
                                               .AsQueryable()
                                               .Project()
                                               .To<ContestBasicDetails>()
