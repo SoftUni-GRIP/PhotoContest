fd4b7cb5ef31186e8405e0d5ce25cbd6b20e9df0
Pe6o601 <petermarinov93@abv.bg>
2015-11-02 14:57:29 -0800

    added delete for picture

--- PhotoContest.Web/Controllers/UploadController.cs
+++ PhotoContest.Web/Controllers/UploadController.cs
@@ -5,6 +5,10 @@
     using System.Linq;
     using System.Web;
     using System.Web.Mvc;
+    using AutoMapper;
+    using Extensions;
+    using Models;
+    using Models.ContestModels.ViewModels;
     using PhotoContest.Data.Contracts;
     using PhotoContest.Models;
     using PhotoContest.Web.Infrastructure.Dropbox;
@@ -19,6 +23,7 @@
         public UploadController(IPhotoContestData data)
             : base(data)
         {
+
         }
 
         [HttpPost]
@@ -33,6 +38,7 @@
                 {
                     ContestId = contestId,
                     UserId = this.CurrentUser.Id,
+                    CreatedOn = DateTime.Now,
                     Url = Dropbox.Download(paths[0])
                 };
                 contest.Pictures.Add(photo);
@@ -42,5 +48,49 @@
 
             return Content("");
         }
+
+        [HttpGet]
+        public ActionResult DeletePicture(int id)
+        {
+            var picture = this.Data.Pictures.Find(id);
+            var model = Mapper.Map<Picture, PictureViewModel>(picture);
+           
+            return this.PartialView("_DeletePicture", model);
+        }
+
+        public JsonResult DeletePicture(PictureViewModel model)
+        {
+            //TODO Validation
+            var picture = this.Data.Pictures.Find(model.Id);
+
+            if (picture != null)
+            {
+                this.DeletePicturetData(picture);
+                this.AddNotification("Contest Edited", NotificationType.SUCCESS);
+                return Json(new { Message = "home" }, JsonRequestBehavior.AllowGet);
+            }
+
+            this.AddNotification("Something is worng. Plase try again", NotificationType.ERROR);
+            return this.Json(new { Message = "error" }, JsonRequestBehavior.AllowGet);
+        }
+
+        private void DeletePicturetData(Picture picture)
+        {
+            var votes = picture.Votes.ToList();
+
+            for (int i = 0; i < votes.Count; i++)
+            {
+                var vote = votes[i];
+                this.Data.Votes.Delete(vote);
+            }
+
+            this.Data.Pictures.Delete(picture);
+
+            this.Data.SaveChanges();
+
+        }
+    
     }
+
+ 
 }
